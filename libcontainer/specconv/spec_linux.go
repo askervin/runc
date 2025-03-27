06415e0c9095b83b81bd21288c21539e50c56914
Antti Kervinen <antti.kervinen@intel.com>
2025-03-27 15:42:24 +0200

    Add memory policy support
    Implement support for Linux memory policy in OCI spec PR:
    https://github.com/opencontainers/runtime-spec/pull/1282
    
    Signed-off-by: Antti Kervinen <antti.kervinen@intel.com>

--- libcontainer/specconv/spec_linux.go
+++ libcontainer/specconv/spec_linux.go
@@ -41,6 +41,14 @@ var (
 		flag  int
 	}
 	complexFlags map[string]func(*configs.Mount)
+	mpolModeMap  map[specs.MemoryPolicyModeType]uint
+	mpolModeFMap map[specs.MemoryPolicyFlagType]uint
+)
+
+const (
+	// maxNumaNode is the maximum NUMA node number supported.
+	// Must be large enough to cover what the kernel supports.
+	maxNumaNode = 4095
 )
 
 func initMaps() {
@@ -148,6 +156,22 @@ func initMaps() {
 				m.IDMapping.Recursive = true
 			},
 		}
+
+		mpolModeMap = map[specs.MemoryPolicyModeType]uint{
+			specs.MpolDefault:            configs.MPOL_DEFAULT,
+			specs.MpolPreferred:          configs.MPOL_PREFERRED,
+			specs.MpolBind:               configs.MPOL_BIND,
+			specs.MpolInterleave:         configs.MPOL_INTERLEAVE,
+			specs.MpolLocal:              configs.MPOL_LOCAL,
+			specs.MpolPreferredMany:      configs.MPOL_PREFERRED_MANY,
+			specs.MpolWeightedInterleave: configs.MPOL_WEIGHTED_INTERLEAVE,
+		}
+
+		mpolModeFMap = map[specs.MemoryPolicyFlagType]uint{
+			specs.MpolFStaticNodes:   configs.MPOL_F_STATIC_NODES,
+			specs.MpolFRelativeNodes: configs.MPOL_F_RELATIVE_NODES,
+			specs.MpolFNumaBalancing: configs.MPOL_F_NUMA_BALANCING,
+		}
 	})
 }
 
@@ -179,6 +203,30 @@ func KnownMountOptions() []string {
 	}
 	for k := range extensionFlags {
 		res = append(res, k)
+	}
+	sort.Strings(res)
+	return res
+}
+
+// KnownMemoryPolicyModes returns the list of the known memory policy modes.
+// Used by `runc features`.
+func KnownMemoryPolicyModes() []string {
+	initMaps()
+	var res []string
+	for k := range mpolModeMap {
+		res = append(res, string(k))
+	}
+	sort.Strings(res)
+	return res
+}
+
+// KnownMemoryPolicyFlags returns the list of the known memory policy mode flags.
+// Used by `runc features`.
+func KnownMemoryPolicyFlags() []string {
+	initMaps()
+	var res []string
+	for k := range mpolModeFMap {
+		res = append(res, string(k))
 	}
 	sort.Strings(res)
 	return res
@@ -466,6 +514,28 @@ func CreateLibcontainerConfig(opts *CreateOpts) (*configs.Config, error) {
 				L3CacheSchema: spec.Linux.IntelRdt.L3CacheSchema,
 				MemBwSchema:   spec.Linux.IntelRdt.MemBwSchema,
 			}
+		}
+		if spec.Linux.MemoryPolicy != nil {
+			var ok bool
+			var err error
+			specMp := spec.Linux.MemoryPolicy
+			confMp := &configs.LinuxMemoryPolicy{}
+			confMp.Mode, ok = mpolModeMap[specMp.Mode]
+			if !ok {
+				return nil, fmt.Errorf("invalid memory policy mode %q", specMp.Mode)
+			}
+			confMp.Nodes, err = configs.ToCPUSet(specMp.Nodes)
+			if err != nil {
+				return nil, fmt.Errorf("invalid memory policy nodes %q: %w", specMp.Nodes, err)
+			}
+			for _, specFlag := range specMp.Flags {
+				confModeFlag, ok := mpolModeFMap[specFlag]
+				if !ok {
+					return nil, fmt.Errorf("invalid memory policy flag %q", specFlag)
+				}
+				confMp.Flags = append(confMp.Flags, confModeFlag)
+			}
+			config.MemoryPolicy = confMp
 		}
 		if spec.Linux.Personality != nil {
 			if len(spec.Linux.Personality.Flags) > 0 {
